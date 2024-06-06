acf57145006ffde56b57bf8e52f5519bfd99e642
max <demouserindeed@gmail.com>
2024-06-06 15:31:14 +0530

    add-error-boundary-component

--- src/main.tsx
+++ src/main.tsx
@@ -4,11 +4,14 @@ import App from './App.tsx';
 import './index.css';
 import { Provider } from 'react-redux';
 import store from './redux/store';
+import ErrorBoundary from './components/ErrorBoundary/ErrorBoundary.tsx';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <Provider store={store}>
-      <App />
-    </Provider>
+    <ErrorBoundary fallback={<p>Something Went Wrong</p>}>
+      <Provider store={store}>
+        <App />
+      </Provider>
+    </ErrorBoundary>
   </React.StrictMode>,
 );
